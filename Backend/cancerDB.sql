4f7be96cf9d14c71b26e805fd7d8011594542546
Maalolan Bharaniraj <maalolan@buffalo.edu>
2023-12-06 18:21:56 -0500

    dump added to backend folder

--- Backend/cancerDB.sql
+++ Backend/cancerDB.sql
@@ -10,29 +10,27 @@ CREATE DATABASE cancerstatisticdb;
 USE cancerstatisticdb;
 
 -- create the tables
--- lol testing
-CREATE TABLE demographicgroup # DO THIS FIRST -- Insert these via created csv
+CREATE TABLE demographicgroup
 (
   groupID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   sex VARCHAR(50) NOT NULL,
   race VARCHAR(50) NOT NULL,
   UNIQUE(sex, race)
-  #UNIQUE SPECIFICATION REMOVED -- the way we have setup the csv as discussed in snell, they cannot be unique since sex and race repeat 5x3, only imports 3 unique records if kept unique.
 );
 
-CREATE TABLE state # DO THIS SECOND -- Insert these via created csv
+CREATE TABLE state
 (
 	sname VARCHAR(64) PRIMARY KEY NOT NULL
 );
 
-CREATE TABLE users # Leave empty for now, will be populated by our frontend
+CREATE TABLE users
 (
     username VARCHAR(50) PRIMARY KEY,
     password_hash VARCHAR(255) NOT NULL,
     role VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE user_action # Further discussion required (what actions can a user take?)
+CREATE TABLE user_action
 (
 	a_type INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 	role_requirement INT,
@@ -40,7 +38,7 @@ CREATE TABLE user_action # Further discussion required (what actions can a user 
 );
 
 
-CREATE TABLE activitylog # Leave empty for now, will be populated by our frontend
+CREATE TABLE activitylog
 (
     logid INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     time_stamp TIMESTAMP NOT NULL,
@@ -51,7 +49,7 @@ CREATE TABLE activitylog # Leave empty for now, will be populated by our fronten
     FOREIGN KEY (log_action) REFERENCES user_action(a_type) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
-CREATE TABLE patient # Fabricate these values
+CREATE TABLE patient
 (
 	id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     name VARCHAR(64) NOT NULL,
@@ -60,7 +58,7 @@ CREATE TABLE patient # Fabricate these values
     FOREIGN KEY (groupID) REFERENCES demographicgroup(groupID) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
-CREATE TABLE professional # Fabricate these values
+CREATE TABLE professional
 (
 	id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     name VARCHAR(64) NOT NULL,
@@ -69,13 +67,13 @@ CREATE TABLE professional # Fabricate these values
 	gender VARCHAR(64) NOT NULL
 );
 
-CREATE TABLE cancertype # DO THIS THIRD -- Insert these via created csv
+CREATE TABLE cancertype
 (
 	name VARCHAR(64) PRIMARY KEY NOT NULL,
 	description VARCHAR(200) NOT NULL
 );
 
-CREATE TABLE report # Fabricate these values
+CREATE TABLE report
 (
 	id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     time_stamp TIMESTAMP NOT NULL,
@@ -87,7 +85,7 @@ CREATE TABLE report # Fabricate these values
     FOREIGN KEY (patient) REFERENCES patient(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
-CREATE TABLE incidencerate # Insert via csv files
+CREATE TABLE incidencerate
 (
 	id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     state VARCHAR(64) NOT NULL,
@@ -100,10 +98,9 @@ CREATE TABLE incidencerate # Insert via csv files
     population INT,
     FOREIGN KEY (state) REFERENCES state(sname) ON UPDATE CASCADE ON DELETE CASCADE,
 	FOREIGN KEY (cancertype) REFERENCES cancertype(name) ON UPDATE CASCADE ON DELETE CASCADE
-    #REMOVED FOREIGN KEY -- REFERENCING demographicgroup, bummer. reason explained in demographics table. We still have an option to refer to that table by refering to the groupID but we need to manually add groupID to 60 csv files. skipping that part atm, lmk.
 );
 
-CREATE TABLE deathrate # Insert via csv files
+CREATE TABLE deathrate
 (
 	id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     state VARCHAR(64) NOT NULL,
@@ -116,7 +113,6 @@ CREATE TABLE deathrate # Insert via csv files
     population INT,
     FOREIGN KEY (state) REFERENCES state(sname) ON UPDATE CASCADE ON DELETE CASCADE,
 	FOREIGN KEY (cancertype) REFERENCES cancertype(name) ON UPDATE CASCADE ON DELETE CASCADE
-    #REMOVED FOREIGN KEY -- REFERENCING demographicgroup, bummer. reason explained in demographics table. We still have an option to refer to that table by refering to the groupID but we need to manually add groupID to 60 csv files. skipping that part atm, lmk.
 );
 
 INSERT INTO state VALUES
